332f9e59dbe2fc340d67ac348ac95e848b1abd36
CHETcica <61766789+CHETcica@users.noreply.github.com>
2022-02-09 11:28:22 +0700

    chetchat

---
+++ src/pages/Userdescriptionpage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Userdescription from "../component/Description"
+
+const Userdescriptionpage = () => {
+  return (
+      <>
+        <Userdescription/>
+      </>
+  );
+};
+
+export default Userdescriptionpage;
