e62408f69478a5a9a571e781c72ba9c32623f27b
CHETcica <61766789+CHETcica@users.noreply.github.com>
2022-02-09 11:29:54 +0700

    Chetchat2

--- src/pages/Chatpage.js
+++ src/pages/Chatpage.js
@@ -1,91 +1,14 @@
 import React from "react";
+import Carduser from "../component/Carduser";
+import Columchat from "../component/Columchat";
+import Chatbox from "../component/Chatbox";
 
 const Chatpage = () => {
   return (
-    <div className="container mx-auto bg-black">
-      <div className="w-96">
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="w-12 h-12 mask mask-squircle">
-                      <img
-                        src="http://daisyui.com/tailwind-css-component-profile-1@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Hart Hagerty</div>
-                    <div className="text-sm opacity-50">United States</div>
-                  </div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="w-12 h-12 mask mask-squircle">
-                      <img
-                        src="http://daisyui.com/tailwind-css-component-profile-3@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Brice Swyre</div>
-                    <div className="text-sm opacity-50">China</div>
-                  </div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="w-12 h-12 mask mask-squircle">
-                      <img
-                        src="http://daisyui.com/tailwind-css-component-profile-4@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Marjy Ferencz</div>
-                    <div className="text-sm opacity-50">Russia</div>
-                  </div>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="w-12 h-12 mask mask-squircle">
-                      <img
-                        src="https://img-07.stickers.cloud/packs/6bbfef47-d910-40fb-8314-7af7be70cf7c/webp/52132f2a-37a1-4beb-b3f6-0f85a8596a89.webp"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Yancy Tear</div>
-                    <div className="text-sm opacity-50">Brazil</div>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    <div className="container mx-auto grid grid-cols-3 w-2/4">
+      <div className="col-span-1"><Columchat /></div>
+      <div className="col-span-2 "><Chatbox /></div>
+      
     </div>
   );
 };
