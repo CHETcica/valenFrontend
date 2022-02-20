e1d36b472cac2ab10caf36f7d3a785039b0e497c
CHETcica <61766789+CHETcica@users.noreply.github.com>
2022-02-20 20:31:08 +0700

    chet V befor meag

--- src/action/index.js
+++ src/action/index.js
@@ -1,6 +1,11 @@
 import reduxType from '../config/reduxType';
 import api from '../api';
 
+export const fetchAllUser = () => async (dispatch) => {
+  const res = await api.get('/user/showuser');
+  console.log(res.data);
+dispatch({ type: reduxType.FETCH_SEARCH_USER, payload: res.data })
+};
 
 export const fetchRandomPhoto = () => async (dispatch) => {
   let photo = localStorage.getItem("photos")
