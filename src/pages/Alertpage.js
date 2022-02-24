f19947c799004bab7e59b5b6cf45a91d538a1f5e
baskodrock <59428813+kietichai@users.noreply.github.com>
2022-02-24 22:32:21 +0700

    bas02

--- src/pages/Alertpage.js
+++ src/pages/Alertpage.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Alertmatch, Alertwarning } from "../component/Alert";
+import { Alertmatch, Alertwarning, Alertmicrophone, Alertcannotconnect, Alertcamera, Alertsuccess, Alertaccountisblock, Alertmessage, Alertinfo, Alerterror } from "../component/Alert";
 
 const Alertpage = () => {
   return (
     <div className="container mx-auto grid sm:grid-cols-1 md:grid-cols-2">
       <Alertmatch/>
       <Alertwarning/>
-      
+      <Alertmicrophone/>
+      <Alertcannotconnect/>
+      <Alertcamera/>
+      <Alertsuccess/>
+      <Alertaccountisblock/>
+      <Alertmessage/>
+      <Alertinfo/>
+      <Alerterror/>
     </div>
   );
 };
