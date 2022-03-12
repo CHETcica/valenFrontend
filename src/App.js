f0c6a59fbeba1aed98ca240b2ac53bc49f14d189
CHETcica <61766789+CHETcica@users.noreply.github.com>
2022-03-12 14:51:25 +0700

    รวมของป่านลงมา

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Menubar from "./component/Menubar";
 import Footer from "./component/Footer";
-
 import Homepage from "./pages/Homepage";
 import Profile from "./pages/Profile";
 import Chatpage from "./pages/Chatpage"
 import Alertpage from "./pages/Alertpage"
 import Regispage from "./pages/Regispage"
 import Login from "./pages/Login"
-
-
-import Profile from "./pages/Profile";
 import EditPersonal from "./pages/EditPersonal";
 import Liking from "./pages/Liking";
 
@@ -20,7 +16,6 @@ import Liking from "./pages/Liking";
 
 const App = () => {
   return (
-
     <BrowserRouter>
         <Navbar />
         <Menubar />
