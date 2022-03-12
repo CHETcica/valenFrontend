5bc8cdcbfcbeb70bfacd3d5fcd0e1b93cea0488f
CHETcica <61766789+CHETcica@users.noreply.github.com>
2022-03-12 13:52:45 +0700

    aumก่อนเอาโค๊ตรวม

--- src/App.js
+++ src/App.js
@@ -16,29 +16,9 @@ function App() {
       <Menubar />
       <EditPersonal />
       <Footer />
-      <Liking />
+        <Liking />
       <Footer />
-      <label htmlFor="my-modal" className="btn modal-button">
-        open modal
-      </label>
-      {/* Put this part before </body> tag */}
-      <input type="checkbox" id="my-modal" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">
-            Congratulations random Interner user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to use
-            Wikipedia for free!
-          </p>
-          <div className="modal-action">
-            <label htmlFor="my-modal" className="btn">
-              Yay!
-            </label>
-          </div>
-        </div>
-      </div>
+      
     </>
   );
 }
