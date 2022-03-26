c1b70b217394d93599ef57c600ccf3e464eabd71
CHETcica <61766789+CHETcica@users.noreply.github.com>
2022-03-26 17:14:29 +0700

    chet function random

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,53 +6,55 @@ import { randomUser } from "../action";
 
 const Homepage = () => {
   const usersignin = useSelector((state) => state.signs);
-  //const users = useSelector((state) => state.users);
+  const users = useSelector((state) => state.users);
 
-  const [users, setUsers] = useState({
-    user: {
-      userDetails: {
-        unlikes: [],
-        vacen: [],
-        taste: [],
-        likes: [],
-        unlike: [],
-        likesMe: [],
-      },
-      _id: "6214a044e543ba936f58d2ea",
-      username: "BOWcica",
-      password: "$2a$12$ZwJaLXxBevU9LJ90OWwb6eehgPVYDgDAVa96QA.4X6TMk13U4h9JW",
-      gender: "woman",
-      born: "2002-02-05T00:00:00.000Z",
-      phone: "0823636036",
-      location: [13.838598, 19.0398371],
-      passion: ["music", "series"],
-      interested: "man",
-      bio: "𝐆𝐎𝐓 𝟕💚",
-      social: {
-        Facebook: "Bow Supansa",
-        Instagram: "𝐛𝐨𝐰𝐥.𝐢𝐧𝐠𝟐𝟎𝟎𝟐",
-      },
-      userimageprofile:
-        "https://cdn.discordapp.com/attachments/792729018608648204/796720785279811614/135865438_224714649180167_1622948695454171292_n.jpg",
-      userimage: [
-        "https://cdn.discordapp.com/attachments/792729018608648204/796720785279811614/135865438_224714649180167_1622948695454171292_n.jpg",
-      ],
-      friendId: ["6213b95e3ee259b511fbcf3e"],
-      __v: 0,
-    },
-  });
+  // const [users, setUsers] = useState({
+  //   user: {
+  //     userDetails: {
+  //       unlikes: [],
+  //       vacen: [],
+  //       taste: [],
+  //       likes: [],
+  //       unlike: [],
+  //       likesMe: [],
+  //     },
+  //     _id: "6214a044e543ba936f58d2ea",
+  //     username: "BOWcica",
+  //     password: "$2a$12$ZwJaLXxBevU9LJ90OWwb6eehgPVYDgDAVa96QA.4X6TMk13U4h9JW",
+  //     gender: "woman",
+  //     born: "2002-02-05T00:00:00.000Z",
+  //     phone: "0823636036",
+  //     location: [13.838598, 19.0398371],
+  //     passion: ["music", "series"],
+  //     interested: "man",
+  //     bio: "𝐆𝐎𝐓 𝟕💚",
+  //     social: {
+  //       Facebook: "Bow Supansa",
+  //       Instagram: "𝐛𝐨𝐰𝐥.𝐢𝐧𝐠𝟐𝟎𝟎𝟐",
+  //     },
+  //     userimageprofile:
+  //       "https://cdn.discordapp.com/attachments/792729018608648204/796720785279811614/135865438_224714649180167_1622948695454171292_n.jpg",
+  //     userimage: [
+  //       "https://cdn.discordapp.com/attachments/792729018608648204/796720785279811614/135865438_224714649180167_1622948695454171292_n.jpg",
+  //     ],
+  //     friendId: ["6213b95e3ee259b511fbcf3e"],
+  //     __v: 0,
+  //   },
+  // });
 
   const disPatch = useDispatch();
   useEffect(() => {
     disPatch(
       // randomUser(
       //   usersignin.user.interested,
-      //   usersignin.user.passion[0],
-      //   usersignin.user.frindId
+      //   usersignin.user.passion,
+      //   usersignin.user.friendId
+      //   usersignin.user.userDetails.likes
+      //   usersignin.user.userDetails.unlike
       // )
-      randomUser("man", "coding", ["6213b95e3ee259b511fbcf3e"])
+      randomUser("man", ["coding","gaming","dance"], ["6213b95e3ee259b511fbcf3e","6216ef7c968ef32ce2e2b49c","623e90c8c457f7c2b1b747e1"])
     );
-  });
+  } );
 
   return (
     <>
